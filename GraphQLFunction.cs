1e17f0344754d9d77bf8efb714c28516e9c840ed
Marco Medrano <marco.medrano.garay@gmail.com>
2022-06-25 15:54:30 -0400

    fet:added logs

--- GraphQLFunction.cs
+++ GraphQLFunction.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
 
 namespace test_azure_function_graphql;
 
@@ -9,9 +10,13 @@ public class GraphQLFunction
 {
     [FunctionName("GraphQLHttpFunction")]
     public Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "graphql/{**slug}")] 
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "graphql/{**slug}")]
         HttpRequest request,
-        [GraphQL] 
-        IGraphQLRequestExecutor executor)
-        => executor.ExecuteAsync(request);
+        [GraphQL]
+        IGraphQLRequestExecutor executor, ILogger log)
+    {
+        log.LogInformation("request: \n" + string.Join(',', request.Headers.Select(h => $"{h.Key}={h.Value}")));
+
+        return executor.ExecuteAsync(request);
+    }
 }
